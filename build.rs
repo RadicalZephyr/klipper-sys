0efd567de5ccea3e61c3df4db1c44aaff3cb23b2
Geoff Shannon <geoffpshannon@gmail.com>
2020-02-06 19:48:32 -0800

    Set up using bindgen to create klipper bindings

--- build.rs
+++ build.rs
@@ -1,9 +1,27 @@
+use std::{env, path::PathBuf};
+
 fn main() {
     cc::Build::new()
+        .warnings(true)
         .file("itersolve.c")
         .file("kin_cartesian.c")
         .file("pyhelper.c")
         .file("serialqueue.c")
         .file("stepcompress.c")
-        .compile("klipper")
+        .compile("klipper");
+
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
---
+++ wrapper.h
@@ -0,0 +1,6 @@
+#include "compiler.h"
+#include "itersolve.h"
+#include "list.h"
+#include "pyhelper.h"
+#include "serialqueue.h"
+#include "stepcompress.h"
